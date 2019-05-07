edf678eaa85e133982700a7c6fa74d2133d6dee0
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-05-07 23:14:08 +0530

    Update Count Module

--- src/app/reports/user-count/user-count.component.ts
+++ src/app/reports/user-count/user-count.component.ts
@@ -3,13 +3,6 @@ import { OrganizerService } from 'src/app/services/organizer.service';
 import { ContributorService } from 'src/app/services/contributor.service';
 import { RobotService } from 'src/app/services/robot.service';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
-
 @Component({
   selector: 'app-user-count',
   templateUrl: './user-count.component.html',
@@ -41,12 +34,5 @@ export class UserCountComponent implements OnInit {
       this.robCount=data;
     })
   }
-
-  tiles: Tile[] = [
-    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
-  ];
 
 }
