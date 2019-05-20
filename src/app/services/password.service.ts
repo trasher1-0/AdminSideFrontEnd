f40d92e6d1f6093dcdb1ab194a1c58e4c34e165f
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-05-20 14:26:24 +0530

    Update Password service

--- src/app/services/password.service.ts
+++ src/app/services/password.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  orgAPI="http://localhost:8080/trasher/api/forget/org";
+  contAPI="http://localhost:8080/trasher/api/forget/org";
+
+  getContributor(email){
+    return this.http.post(this.contAPI,{
+      'email':email
+    });
+  }
+
+  getOrganizer(email){
+    return this.http.post(this.orgAPI,{
+      'email':email
+    });
+  }
 }
